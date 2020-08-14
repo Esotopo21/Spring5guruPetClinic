1d5abf415c0e5b093e41418b9036c7eedd41e247
burlac <cristiano.burla@prometeia.com>
2020-08-14 10:01:15 +0200

    Add jpa services

---
+++ pet-clinic-web/src/main/java/sfgpetclinic/services/jpa/VisitJpaService.java
@@ -0,0 +1,48 @@
+package sfgpetclinic.services.jpa;
+
+import it.burlac.sfgpetclinic.model.Visit;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import sfgpetclinic.repository.VisitRepository;
+import sfgpetclinic.services.VisitiService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpaservice")
+public class VisitJpaService implements VisitiService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
